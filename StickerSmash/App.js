00bcc30b7d14b83a174fbc667925f72a292ce18a
Eduardo <eduardonunes8349@gmail.com>
2024-03-13 12:23:31 -0300

    Criando as variaveis de estado

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,20 +1,55 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import ImageViewer from './src/components/ImageViewer';
 import Button from './src/components/Button';
 import * as ImagePicker from 'expo-image-picker';
 
+
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    }
+    else {
+      alert("Você não selecionou uma imagem");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer 
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage} 
+        />
       </View>
+      {showAppOptions ? (
+        <View/>
+      ) : (
+        <View style={styles.footerContainer}>
+          </View>
+      )}
       <View style={styles.footerContainer}>
-        <Button label="Escolher Foto" theme="primary" />
-        <Button label="Usar esta Foto" />
+        <Button
+          label="Escolher Foto"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
+        <Button 
+          label="Usar esta Foto"
+          onPress={() => setShowAppOptions(true)} 
+        />
       </View>
       <StatusBar style="auto" />
     </View>
